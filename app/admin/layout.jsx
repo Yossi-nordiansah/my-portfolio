246749e5d9d4565b23d939c99c2c6277ed3e0e86
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2025-05-05 19:29:56 +0700

    informasi periode selesai

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -19,19 +19,19 @@ const AdminLayout = ({ children }) => {
             <div className='absolute bg-primary h-full w-48 pt-24 z-10 text-white font-semibold'>
                 <p className='text-xl mb-10 px-3'>nama admin</p>
                 <ul className='flex-col'>
-                    <Link href="/admin/pelatihan" className={`py-4 border-t block px-3 ${pathName === "/admin/pelatihan" ? "bg-yellow-400" : ""}`}>
+                    <Link href="/admin/pelatihan" className={`py-4 border-y block px-3 ${pathName === "/admin/pelatihan" ? "bg-yellow-400" : ""}`}>
                         <li className='flex justify-between'>
                             <p>Pelatihan</p>
                             <img src="/icons/triangle.svg" alt="" className='w-5 rotate-90' />
                         </li>
                     </Link>
-                    <Link href="/admin/informasi-periode" className='py-4 border-y block px-3'>
+                    <Link href="/admin/informasi-periode" className={`py-4 border-b block px-3 ${pathName === "/admin/informasi-periode" ? "bg-yellow-400" : ""}`}>
                         <li className='flex justify-between'>
                             <p>Informasi Periode</p>
                             <img src="/icons/triangle.svg" alt="" className='w-5 rotate-90' />
                         </li>
                     </Link>
-                    <Link href="/admin/jadwal" className='py-4 border-b block px-3'>
+                    <Link href="/admin/jadwal" className={`py-4 border-b block px-3 ${pathName === "/admin/jadwal" ? "bg-yellow-400" : ""}`}>
                         <li className='flex justify-between'>
                             <p>Jadwal</p>
                             <img src="/icons/triangle.svg" alt="" className='w-5 rotate-90' />
